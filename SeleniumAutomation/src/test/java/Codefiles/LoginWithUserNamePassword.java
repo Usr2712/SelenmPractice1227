f6f6801331eecaf92fd16da4981dfc527f0fce07
Usr2712 <Raghavendrasai106@gmail.com>
2024-05-08 19:38:03 +0530

    New Commit

---
+++ SeleniumAutomation/src/test/java/Codefiles/LoginWithUserNamePassword.java
@@ -0,0 +1,38 @@
+package Codefiles;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class LoginWithUserNamePassword extends Logincode {
+
+WebDriver driver;
+
+    @FindBy(xpath = "//input[@id='txt-username']")
+    static WebElement UserName;
+
+    @FindBy(xpath = "//input[@id='txt-password']")
+    static WebElement Password;
+
+    @FindBy(xpath = "//button[contains(text(),'Login')]")
+    static WebElement Login;
+
+    public LoginWithUserNamePassword(WebDriver driver) throws IOException {
+    this.driver=driver;
+        PageFactory.initElements(driver,this);
+    }
+
+
+    @Test
+    public void Enter_Username_Password_and_login(){
+
+        UserName.sendKeys("John Doe");
+        Password.sendKeys("ThisIsNotAPassword");
+        Login.click();
+    }
+}
